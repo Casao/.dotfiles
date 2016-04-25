9676d2e1f6ee50491069439a53c5697ed9660a6a
Colin Ewen <colin@draecas.com>
2016-04-25 13:30:42 -0400

    Install sublime text

--- hooks/post-up/homebrew-apps.sh
+++ hooks/post-up/homebrew-apps.sh
@@ -3,3 +3,12 @@
 if [ ! brew list --versions trash ]; then
   brew install trash
 fi
+
+if [ ! brew list --versions cask ]; then
+  brew install caskroom/cask/brew-cask
+  brew tap --full caskroom/versions
+fi
+
+if [ ! brew cask list --versions sublime-text3 ]; then
+  brew cask install sublime-text3
+fi
