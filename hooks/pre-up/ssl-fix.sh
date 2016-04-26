e51ee05033ce3e914451f3bfbc3cec8769f4d04d
Colin Ewen <colin@draecas.com>
2016-04-26 11:25:56 -0400

    Updates for new owrk laptop

--- hooks/pre-up/ssl-fix.sh
+++ hooks/pre-up/ssl-fix.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-if [ ! -f $SSL_CERT_FILE ]; then
-  security find-certificate -a -p /Library/Keychains/System.keychain > "$SSL_CERT_FILE"
-  security find-certificate -a -p /System/Library/Keychains/SystemRootCertificates.keychain >> "$SSL_CERT_FILE"
-fi
+security find-certificate -a -p > "$SSL_CERT_FILE"
+security find-certificate -a -p /Library/Keychains/System.keychain >> "$SSL_CERT_FILE"
+security find-certificate -a -p /System/Library/Keychains/SystemRootCertificates.keychain >> "$SSL_CERT_FILE"
