e713932653f5b00eaf17be34eaf452024cc496c8
Colin Ewen <colin@draecas.com>
2016-05-02 16:41:15 -0400

    Add less open, ssl fix

--- hooks/pre-up/ssl-fix.sh
+++ hooks/pre-up/ssl-fix.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SSL_CERT_FILE="${SSL_CERT_FILE:-${HOME}/cert_file.pem}"
+
+touch $SSL_CERT_FILE
+
 security find-certificate -a -p > "$SSL_CERT_FILE"
 security find-certificate -a -p /Library/Keychains/System.keychain >> "$SSL_CERT_FILE"
 security find-certificate -a -p /System/Library/Keychains/SystemRootCertificates.keychain >> "$SSL_CERT_FILE"
