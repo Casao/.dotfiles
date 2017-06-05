5941b09057683e4912c1727c9697438d42dcca53
Colin Ewen <colin@draecas.com>
2017-06-05 08:10:57 -0400

    Fix hooks

--- hooks/post-up/vim.sh
+++ hooks/post-up/vim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-if [ ! -d "${HOME}/.zsh/bundle/Vundle.vim" ]; then
+if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
